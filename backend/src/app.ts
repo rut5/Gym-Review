2270c420dfce7152a767a77307537267b70c467c
Mina R <minarumang@icloud.com>
2026-05-11 19:24:10 +0200

    Add tests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import profileRoutes from "./routes/profileRoutes.ts";
-import errorMiddleware from "./middleware/errorMiddleware.ts";
-import placeRoutes from "./routes/placeRoutes.ts";
+import profileRoutes from "./routes/profileRoutes.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
+import placeRoutes from "./routes/placeRoutes.js";
 
 const app = express();
 const clientOrigin = process.env.CLIENT_ORIGIN ?? "http://localhost:5173";
