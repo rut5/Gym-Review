18ced7a68e7a23b131bba5d55ced48ce5f593d0b
LoS <lostreit@pm.me>
2026-05-12 13:25:30 +0200

    Fix: removed fetch data test

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,28 +1,8 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import axios from "axios";
-import { useState } from "react";
 import Login from "./Login";
 
 function Profile() {
-  const { isAuthenticated, user, logout, isLoading, getAccessTokenSilently } =
-    useAuth0();
-
-  const [secureData, setSecureData] = useState(null);
-  const [fetchError, setFetchError] = useState(null);
-
-  const fetchData = async () => {
-    try {
-      const token = await getAccessTokenSilently();
-      const response = await axios.get(`/api/secure-data`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setSecureData(response.data.message);
-    } catch (err) {
-      setFetchError(err.response?.data?.message || err.message);
-    }
-  };
+  const { isAuthenticated, user, logout, isLoading } = useAuth0();
 
   return (
     <>
@@ -55,9 +35,6 @@ function Profile() {
             <p>Username: {user?.name}</p>
             <p>Email: {user?.email}</p>
             <button onClick={() => logout()}>Logout</button>
-            <button onClick={() => fetchData()}>Fetch secure data</button>
-            {secureData && <p>{secureData}</p>}
-            {fetchError && <p style={{ color: "red" }}>{fetchError}</p>}
           </div>
         ) : (
           <Login />
